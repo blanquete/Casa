1157f70da8372a271b3f68b2b6eb5e9bc99ce403
Raul <rblanco.dam@institutcampalans.net>
2021-11-12 00:47:57 +0100

    Subida de archivos, Conexion BBDD

---
+++ Cliente.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public string nombre { get; set; }
+        
+        public Cliente() { }
+
+        public Cliente(int pId, string pNombre)
+        {
+            this.ID = pId;
+            this.nombre = pNombre;
+        }
+        public  int Agregar(Cliente pCliente)
+        {
+            int retorno = 0;
+            MySqlCommand comando = new MySqlCommand(string.Format("Insert into  persona(ID, nombre) values('{0}','{1}')", pCliente.ID, pCliente.nombre), connectionBBDD.ObtenerConexion());
+
+            retorno = comando.ExecuteNonQuery();
+
+            return retorno;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            connectionBBDD.ObtenerConexion();
+            Cliente c = new Cliente();
+
+            c.ID = 1;
+            c.nombre = "Raul";
+
+            int resultado = c.Agregar(c);
+
+
+            Console.ReadKey();
+        }
+    }
+
+}
